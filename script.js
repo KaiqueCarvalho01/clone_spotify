fa0427aa2e58af19a8d89f77d94c64d4af520b1c
Kaique Carvalho <kaiquecarvalho479jogos@gmail.com>
2025-02-06 09:58:27 -0300

    Refactor artist data handling and update DOM manipulation for artist cards

--- script.js
+++ script.js
@@ -5,18 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
     //Um array de objetos de artistas, onde cada objeto terá as propriedades nome e imagem.
     const artistsData = [
 
-        { name: 'Henrique & Juliano', image: './assets/artista-henrique-juliano.jpg' },
-        
+        { name: 'Henrique & Juliano', image: './assets/artista-henrique-juliano.jpg' }, 
         { name: 'Jorge & Mateus', image: './assets/artista-jorge-mateus.jpg' },
         { name: 'Zé Neto & Cristiano', image: './assets/artista-ze-neto.jpg' },
-        { name: 'Gusttavo Lima', image: './assets/artista-gustavo-limma.jpg'
-        },
+        { name: 'Gusttavo Lima', image: './assets/artista-gustavo-limma.jpg'},
         { name: 'Luan Santana', image: './assets/artista-luan-santana.jpg' },
-        
         { name: 'Matheus & Kauan', image: './assets/artista-mateus- kauan.jpg' }
-        
         ];
-        const albumsData = [
+
+    const albumsData = [
         { name: 'White Noise (Sleep & Relaxation Sounds)', artist: 'SleepyJohn', image: './assets/album-white-noise.jpg' },
         { name: 'O Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano',
         image: './assets/album-ceu-explica.jpg' },
@@ -30,8 +27,24 @@ document.addEventListener('DOMContentLoaded', () => {
         ];
 
         //Selecionando o elemento com a classe artist-grid
-        const artistGrid = document.querySelector('.artist-grid'); //M
-        const albumGrid = document.querySelector('.albums-grid'); //M
+        const artistsGrid = document.querySelector('.artists-grid'); //M
+        const albumsGrid = document.querySelector('.albums-grid'); //M
+
+        //Pegando item por item
+        artistsData.forEach(artist => {
+            //Criando um elemento div
+            const artistCard = document.createElement('div')
+            //Adicionando a classe artist-card ao elemento
+            artistCard.classList.add('artist-card')
+            //Adicionando o HTML do artista ao elemento
+            artistElement.innerHTML = `
+            <img src="${artist.image}" alt="${artist.name}">
+            <h3>${artist.name}</h3>
+            <p>artista</p>
+            `
+            //Adicionando o elemento ao artistGrid
+            artistsGrid.appendChild(artistCard)
+        })
 
 })
 
